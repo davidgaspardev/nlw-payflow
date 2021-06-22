7c2ee2bb03bbec122c728692e48e7f55bc6efb9b
David Gaspar <davidgaspar.dev@gmail.com>
2021-06-22 15:20:21 -0300

    Provided access to images - close #5

---
+++ lib/app/shared/themes/images.dart
@@ -0,0 +1,11 @@
+/// All image paths used by the app
+class ImagePaths {
+
+  // Images png
+  static const google = "lib/app/assets/images/png/google.png";
+  static const logoFull = "lib/app/assets/images/png/logofull.png";
+  static const logomini = "lib/app/assets/images/png/logomini.png";
+  static const person = "lib/app/assets/images/png/person.png";
+  static const union = "lib/app/assets/images/png/union.png";
+
+}
