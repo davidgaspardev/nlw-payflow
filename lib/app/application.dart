2aed8b7e587fc2401ad23ace891a8702062d4f87
David Gaspar <davidgaspar.dev@gmail.com>
2021-06-24 00:25:54 -0300

    Firebase core imeplemented - close #12

---
+++ lib/app/application.dart
@@ -0,0 +1,28 @@
+/// External package
+import 'package:flutter/material.dart';
+/// Internal packages
+import 'package:payflow/app/helpers/dimensions.dart';
+import 'package:payflow/app/pages/login/login_page.dart';
+import 'package:payflow/app/pages/splash/splash_page.dart';
+import 'package:payflow/app/shared/themes/palette.dart';
+
+class Application extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PayFlow',
+      theme: ThemeData(
+        primaryColor: ColorPalette.primary
+      ),
+      initialRoute: LoginPage.routeName,
+      routes: {
+        SplashPage.routeName: (BuildContext context) {
+          Dimensions.init(context);
+          return SplashPage();
+        },
+        LoginPage.routeName: (BuildContext context) => LoginPage(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,54 @@
+/// External package
 import 'package:flutter/material.dart';
-import 'package:payflow/app/helpers/dimensions.dart';
-import 'package:payflow/app/pages/login/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:payflow/app/application.dart';
 import 'package:payflow/app/pages/splash/splash_page.dart';
-import 'package:payflow/app/shared/themes/palette.dart';
+import 'package:payflow/app/shared/themes/styles.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
-  // This widget is the root of your application.
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PayFlow',
-      theme: ThemeData(
-        primaryColor: ColorPalette.primary
-      ),
-      initialRoute: LoginPage.routeName,
-      routes: {
-        SplashPage.routeName: (BuildContext context) {
-          Dimensions.init(context);
-          return SplashPage();
-        },
-        LoginPage.routeName: (BuildContext context) => LoginPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container(
+            alignment: Alignment.center,
+            child: Text(
+              "Ocorreu algum error com o Firebase",
+              style: TextStyles.titleHome
+            ),
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Application();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return SplashPage();
       },
     );
   }
