791902a6005674d4ab4c4b1db985b38d8525d291
David Gaspar <davidgaspar.dev@gmail.com>
2021-06-24 01:21:26 -0300

    Empty home page added

---
+++ lib/app/pages/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget { 
+
+  static final routeName = "/home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
