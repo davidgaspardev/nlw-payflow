07d07c2dbdbaa26c88de45b8f3922282b10530be
David Gaspar <davidgaspar.dev@gmail.com>
2021-11-02 01:43:46 -0300

    WidgetsBindingObserver implemented

--- lib/app/shared/widgets/provider.dart
+++ lib/app/shared/widgets/provider.dart
@@ -33,7 +33,10 @@ class Provider<T extends Controller> extends StatefulWidget {
 
 /// Provider state links the lifecycle methods with the controller, thus
 /// abstracting business logic outside of visualization.
-class _ProviderState extends State<Provider> {
+class _ProviderState extends State<Provider> with WidgetsBindingObserver {
+  void _log(String method, { String? message }) {
+    print("($hashCode:${controller.hashCode}) [ Provider | $method ] ${message != null ? message : ""}");
+  }
 
   Page get page => this.widget.page;
   Controller get controller => this.widget.controller;
@@ -46,13 +49,17 @@ class _ProviderState extends State<Provider> {
     // Initialize controller with context
     controller.init(context);
     // Add callback to first build
-    WidgetsBinding.instance!.addPostFrameCallback((Duration timeStamp) {
+    WidgetsBinding.instance!.addPostFrameCallback((Duration timestamp) {
+      _log("addPostFrameCallback", message: "timestamp: $timestamp");
       controller.builded();
     });
+    WidgetsBinding.instance!.addObserver(this);
+    _log("initState");
   }
 
   @override
   Widget build(BuildContext context) {
+    _log("build");
     // Build/update page
     controller.build();
     // Retun page
@@ -64,10 +71,45 @@ class _ProviderState extends State<Provider> {
   }
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    _log("didChangeAppLifecycleState", message: "state: $state");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+
+    _log("deactivate");
+  }
+
+  @override
+  Future<bool> didPopRoute() {
+    _log("didPopRoute");
+    return super.didPopRoute();
+  }
+
+  @override
+  Future<bool> didPushRoute(String route) {
+    _log("didPushRoute", message: "route: $route");
+    return super.didPushRoute(route);
+  }
+
+  @override
+  Future<bool> didPushRouteInformation(RouteInformation routeInformation) {
+    _log("didPushRouteInformation", message: "routeInformation: $routeInformation");
+    return super.didPushRouteInformation(routeInformation);
+  }
+
+  @override
   void dispose() {
-    // Dispose controller
-    controller.dispose();
+    _log("dispose");
     // Dispose widget
     super.dispose();
+    // Dispose controller
+    controller.dispose();
+
+    WidgetsBinding.instance!.removeObserver(this);
   }
 }
